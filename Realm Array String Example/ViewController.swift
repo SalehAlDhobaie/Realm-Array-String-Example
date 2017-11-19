2237a2f02b58b605e421c3775eb766d96eedd41c
Saleh AlDhobaie <Saleh.Dhobaie@gmail.com>
2017-11-19 11:24:38 +0300

    Initial Commit

---
+++ Realm Array String Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realm Array String Example
+//
+//  Created by Saleh AlDhobaie on 11/19/17.
+//  Copyright © 2017 Saleh AlDhobaie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
